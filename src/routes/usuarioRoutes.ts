eb75c8dd33613ceef19e5371d70e4c3df8c74ffa
Gabriel Carmo <gabrielcarmobr19@gmail.com>
2025-04-29 15:06:33 -0300

    Add route to retrieve user by ID

--- src/routes/usuarioRoutes.ts
+++ src/routes/usuarioRoutes.ts
@@ -10,5 +10,5 @@ router.post('/', (req, res, next) => {
 });
 router.put('/:id', UsuarioController.update);
 router.delete('/:id', UsuarioController.delete);
-
+router.get('/:id', UsuarioController.getById); // <-- Corrigido e adicionado
 export default router;
