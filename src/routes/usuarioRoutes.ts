6397621c1fe718d8a495506d20fbd9cfe669fc7f
Gabriel Carmo <gabrielcarmobr19@gmail.com>
2025-05-20 09:38:37 -0300

    Refactor usuarioRoutes to enhance image upload handling for user creation and updates; ensure proper middleware usage for file uploads.

--- src/routes/usuarioRoutes.ts
+++ src/routes/usuarioRoutes.ts
@@ -1,14 +1,17 @@
 import { Router } from 'express';
 import { UsuarioController } from '../controllers/UsuarioController';
-
+import { upload } from '../uploads/uploads';
 const router = Router();
 
 // Definindo as rotas com os controladores
 router.get('/', UsuarioController.getAll);
-router.post('/', (req, res, next) => {
+router.post('/', upload.single('foto'), (req, res, next) => {
   UsuarioController.create(req, res, next);
 });
-router.put('/:id', UsuarioController.update);
+router.put('/:id', upload.single('foto'), (req, res, next) => {
+  UsuarioController.update(req, res, next);
+});
+
 router.delete('/:id', UsuarioController.delete);
 router.get('/:id', UsuarioController.getById); // <-- Corrigido e adicionado
 export default router;
